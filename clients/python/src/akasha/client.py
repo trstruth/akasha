698b0b4b9919fe8dea357e93a4448c0f7834c40c
Tristan Struthers <trstruthers@microsoft.com>
2024-09-26 10:55:28 -0700

    Update python client implementation

--- clients/python/src/akasha/client.py
+++ clients/python/src/akasha/client.py
@@ -1,6 +1,6 @@
 import grpc
-from akasha.gen.akasha_pb2_grpc import EvaluationServiceStub
-from akasha.gen.akasha_pb2 import EvaluateStringFlagRequest, EvaluateBoolFlagRequest
+from .gen.akasha_pb2_grpc import EvaluationServiceStub
+from .gen.akasha_pb2 import EvaluateStringFlagRequest, EvaluateBoolFlagRequest, Context
 
 class Client:
     def __init__(self, host: str, port: int):
@@ -9,16 +9,22 @@ class Client:
 
     def get_bool(self, name: str, targets: dict[str, str], default: bool) -> bool:
         with grpc.insecure_channel(f"{self.host}:{self.port}") as channel:
-            stub = EvaluationServiceStub(channel)
-            response = stub.EvaluateBoolFlag(EvaluateBoolFlagRequest(name=name, context=targets))
-            return response.value
+            try:
+                stub = EvaluationServiceStub(channel)
+                response = stub.EvaluateBoolFlag(EvaluateBoolFlagRequest(name=name, context=Context(attributes=targets)))
+                return response.value
+            except grpc.RpcError as e:
+                print(f'got rpc error: {e}')
 
         return default
 
     def get_string(self, name: str, targets: dict[str, str], default: str) -> str:
         with grpc.insecure_channel(f"{self.host}:{self.port}") as channel:
-            stub = EvaluationServiceStub(channel)
-            response = stub.EvaluateStringFlag(EvaluateStringFlagRequest(name=name, context=targets))
-            return response.value
+            try:
+                stub = EvaluationServiceStub(channel)
+                response = stub.EvaluateStringFlag(EvaluateStringFlagRequest(name=name, context=Context(attributes=targets)))
+                return response.value
+            except grpc.RpcError as e:
+                print(f'got rpc error: {e}')
 
         return default
--- clients/python/src/akasha/gen/akasha_pb2_grpc.py
+++ clients/python/src/akasha/gen/akasha_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import akasha_pb2 as akasha__pb2
+from . import akasha_pb2 as akasha__pb2
 
 GRPC_GENERATED_VERSION = '1.66.1'
 GRPC_VERSION = grpc.__version__
