e0c9796e400795c0bf30f78287ae99058b3e2e49
Shivam Vohra <shivamvohra@akasha-dev-1.sncjlzsgcrwu1obrsgwwy0y5qb.dx.internal.cloudapp.net>
2024-10-15 13:57:45 +0000

    reverted main

--- backend/src/metrics/mod.rs
+++ backend/src/metrics/mod.rs
@@ -1,4 +1,1 @@
 pub mod prelude;
-pub mod in_memory;
-
-pub use in_memory::InMemoryMetricsProvider;
--- backend/src/metrics/prelude.rs
+++ backend/src/metrics/prelude.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
-use std::fmt::Debug;
 use std::hash::Hash;
-use async_trait::async_trait;
 
 use tokio::sync::RwLock;
 
@@ -68,8 +66,31 @@ impl From<&str> for VariantType {
     }
 }
 
-#[async_trait]
-pub trait MetricsProvider: Send + Sync + Debug {
-    async fn get_metrics(&self, flag_id: &str) -> Option<MetricsData>;
-    async fn increment_variant(&mut self, flag_id: &str, variant: VariantType);
+#[derive(Debug)]
+pub struct InMemoryMetricsProvider {
+    flags: RwLock<HashMap<String, MetricsData>>,
+}
+
+impl InMemoryMetricsProvider {
+    pub fn new() -> Self {
+        Self {
+            flags: RwLock::new(HashMap::new()),
+        }
+    }
+
+    pub async fn get_metrics(&self, flag_id: &str) -> Option<MetricsData> {
+        self.flags.read().await.get(flag_id).cloned()
+    }
+
+    pub async fn increment_variant(&mut self, flag_id: &str, variant: VariantType) {
+        let mut flags_writer = self.flags.write().await;
+        let data = flags_writer.entry(flag_id.to_string()).or_default();
+        data.increment_variant(variant);
+    }
+}
+
+impl Default for InMemoryMetricsProvider {
+    fn default() -> Self {
+        Self::new()
+    }
 }
